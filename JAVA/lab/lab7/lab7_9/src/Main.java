408aa26c23407fa6f94364e0d640370943b2400e
trieudatly <lytrieudat@gmail.com>
2023-07-26 22:25:08 +0700

    lab7_9

--- JAVA/lab/lab7/lab7_9/src/Main.java
+++ JAVA/lab/lab7/lab7_9/src/Main.java
@@ -2,11 +2,24 @@ import java.util.Scanner;
 
 //Cho một mảng 2 chiều các số nguyên n hàng m cột arr được nhập từ bàn phím.
 // Bạn hãy viết chương trình tính tổng các phần tử trong mảng arr.
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[][] arr = new int[n][m];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int sumArr = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                //System.out.println("arr[" + i + "][" + j + "] = " + arr[i][j]);
+                sumArr += arr[i][j];
+            }
+        }
+        System.out.println(sumArr);
     }
 }
