51508426a32b6efc9af7435fbf65fd55ec18fccf
trieudatly <lytrieudat@gmail.com>
2023-08-02 20:41:50 +0700

    java asm1 start

---
+++ JAVA/PRO192x_ASM_datltfx22205/vn.funix.fx22205/asm01/Asm01.java
@@ -0,0 +1,11 @@
+package asm01;
+
+import java.util.Scanner;
+
+public class Asm01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final String AUTHOR = "fx22205";
+        final String VERSION = "1.0.0";
+    }
+}
