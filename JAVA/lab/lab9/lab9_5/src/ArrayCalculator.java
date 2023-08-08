fa326512aac1a47d10f15ba87af77816eda7ad18
trieudatly <lytrieudat@gmail.com>
2023-08-09 00:49:12 +0700

    java lab9_8

---
+++ JAVA/lab/lab9/lab9_5/src/ArrayCalculator.java
@@ -0,0 +1,18 @@
+public class ArrayCalculator {
+    public static int sumOfArray(int arr[]) {
+        int sum = 0;
+        for (int item : arr) {
+            sum += item;
+        }
+        return sum;
+    }
+
+    public static double sumOfArray(double arr[]) {
+        double sum = 0;
+        for (double item : arr
+        ) {
+            sum += item;
+        }
+        return sum;
+    }
+}
---
+++ JAVA/lab/lab9/lab9_5/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = new int[] {3, 4, 2};
+        double[] arr2 = new double[] {1.3, 4.2, 6.7};
+        System.out.println(ArrayCalculator.sumOfArray(arr1));
+        System.out.println(ArrayCalculator.sumOfArray(arr2));
+    }
+}
