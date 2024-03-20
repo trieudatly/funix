ef8875053f4d6fa594984bd1f52d7cf357d73cd3
trieudatly <lytrieudat@gmail.com>
2024-03-21 00:58:19 +0700

    test ok

--- JAVA/com.trieudat.pro192x31VN.asm4/src/models/GsonTester.java
+++ JAVA/com.trieudat.pro192x31VN.asm4/src/models/GsonTester.java
@@ -29,42 +29,11 @@ public class GsonTester {
         List<Customer> customers=digitalBank.getAllCustomer();
         for (Customer cus:customers
              ) {
-            cus.displayInformation();
+            activeBank.addCustomer(cus);
         }
 
     }
 }
 
-class Student {
-    private String name;
-    private int age;
 
-    public Student() {
-    }
-
-    public Student(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String toString() {
-        return "Student [ name: " + name + ", age: " + age + " ]";
-    }
-}
 
