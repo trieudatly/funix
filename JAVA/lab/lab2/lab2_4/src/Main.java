d8a6821c166a27bd8980458328bb29832415c8a0
trieudatly <lytrieudat@gmail.com>
2024-04-25 20:45:57 +0700

    java 25-04-2024-2

--- JAVA/lab/lab2/lab2_4/src/Main.java
+++ JAVA/lab/lab2/lab2_4/src/Main.java
@@ -1,12 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String s1=sc.next();
-        String s2=sc.next();
-        s1=s1.toLowerCase();
-        s2=s2.toLowerCase();
-        System.out.println(s1.indexOf(s2));
+        String name="Codelearn";
+        System.out.println("Hello " + name);
     }
 }
