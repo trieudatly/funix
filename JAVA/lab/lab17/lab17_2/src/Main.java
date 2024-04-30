28c0ad35b984295576ed9e60b71d32768e6655b4
trieudatly <lytrieudat@gmail.com>
2024-04-30 21:55:12 +0700

    30-4-2024-2

---
+++ JAVA/lab/lab17/lab17_2/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        final BankAccount account = new BankAccount("12345-678", 1000.00);
+        System.out.println(account);
+        Thread trThread1 = new Thread() {
+            public void run() {
+                System.out.println("trThread1 running");
+                account.deposit(300);
+                account.withdraw(50);
+                //System.out.println(account);
+            }
+        };
+        Thread trThread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("trThread2 running");
+                account.deposit(203.75);
+                account.withdraw(100);
+                //System.out.println(account);
+            }
+        });
+
+        trThread1.start();
+        trThread2.start();
+        trThread1.join();
+        trThread2.join();
+        System.out.println(account);
+
+    }
+}
