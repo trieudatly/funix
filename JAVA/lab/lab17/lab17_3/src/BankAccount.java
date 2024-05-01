8115a76d3eac84c24e04e853dfeb4ea6c2d5d938
trieudatly <lytrieudat@gmail.com>
2024-05-02 00:23:05 +0700

    1-5-2024-1

---
+++ JAVA/lab/lab17/lab17_3/src/BankAccount.java
@@ -0,0 +1,53 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BankAccount {
+    private double balance;
+    private String accountNumber;
+    private Lock lock;
+
+    public BankAccount(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+        this.lock=new ReentrantLock();
+    }
+
+    public synchronized void deposit(double amount) {
+//        try {
+//            Thread.sleep(100);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+        double origBalance = balance;
+        balance += amount;
+        System.out.printf("STARTING BALANCE: %.0f, DEPOSIT (%.0f)" +
+                " : NEW BALANCE = %.0f%n", origBalance, amount, balance);
+    }
+
+    public synchronized void withdraw(double amount) {
+//        try {
+//            Thread.sleep(100);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+        double origBalance = balance;
+
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.printf("STARTING BALANCE: %.0f, WITHDRAWAL (%.0f)" +
+                    " : NEW BALANCE = %.0f%n", origBalance, amount, balance);
+        } else {
+            System.out.printf("STARTING BALANCE: %.0f, WITHDRAWAL (%.0f)" +
+                    " : INSUFFICIENT FUNDS!", origBalance, amount);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "balance=" + balance +
+                ", accountNumber='" + accountNumber + '\'' +
+                '}';
+    }
+}
